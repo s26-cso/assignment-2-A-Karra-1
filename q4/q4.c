5360801b06837655599e2829a8e6324feeba2219
Aranya Karra <aranyakarra08@gmail.com>
2026-04-11 02:12:20 +0530

    Done question 4

--- q4/q4.c
+++ q4/q4.c
@@ -1,62 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <dlfcn.h>
 
-int main(void) {
-    char op[6];         /* op is at most 5 chars + null terminator */
-    int num1, num2;
+/*
 
-    char current_op[6] = "";   /* tracks which library is currently loaded */
-    void *handle = NULL;        /* handle to the currently loaded library */
+basically first take op num1 num2 as input then check if already the previous op was same.
+if not then close the curr library
+then use RTLD_LAZY flag while opening so that the library is only looked up if its function is mention
+else it remains unresolved which is better
+
+then by function pointers
+dlsym returns the address of the required function and converts it into the general format which we have typedef here
+so that it can be used for printing
+
+
+*/
+
+int main(void) {
+    char op[6];
+    int num1, num2;
+    char current_op[6] = "";
+    void *handle = NULL;
 
     while (scanf("%5s %d %d", op, &num1, &num2) == 3) {
-
-        /* Only load a new library if the operation has changed */
         if (strcmp(op, current_op) != 0) {
 
-            /* Unload the previous library before loading the new one.
-               This ensures we never hold more than one 1.5GB library
-               in memory at a time, keeping us within the 2GB limit. */
-            if (handle != NULL) {
-                dlclose(handle);
-                handle = NULL;
-            }
-
-            /* Build the shared library filename: lib<op>.so */
-            char libname[16]; /* "lib" (3) + 5 + ".so" (3) + '\0' (1) = 12, 16 is safe */
+            if (handle != NULL) dlclose(handle);
+            char libname[20];
             snprintf(libname, sizeof(libname), "./lib%s.so", op);
 
             handle = dlopen(libname, RTLD_LAZY);
-            if (handle == NULL) {
-                fprintf(stderr, "Error loading library %s: %s\n", libname, dlerror());
-                return 1;
-            }
-
-            strncpy(current_op, op, sizeof(current_op) - 1);
-            current_op[sizeof(current_op) - 1] = '\0';
+            strncpy(current_op, op, 5);
+            current_op[5] = '\0';
         }
-
-        /* Look up the function with the same name as the operation */
-        dlerror(); /* clear any existing error */
 
         typedef int (*op_func_t)(int, int);
+        // function pointer mentioned raaaaahhhhhh
         op_func_t func = (op_func_t) dlsym(handle, op);
-
-        char *err = dlerror();
-        if (err != NULL) {
-            fprintf(stderr, "Error finding symbol %s: %s\n", op, err);
-            dlclose(handle);
-            return 1;
-        }
-
-        int result = func(num1, num2);
-        printf("%d\n", result);
+        printf("%d\n", func(num1, num2));
     }
 
-    if (handle != NULL) {
-        dlclose(handle);
-    }
+    if (handle != NULL) dlclose(handle);
 
     return 0;
 }
